ca9e3b433894f14142260fe57a0a5c307e8c8215
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-08 17:15:43 +0000

    Automatically backed up by Learn

--- book.rb
+++ book.rb
@@ -7,3 +7,18 @@ class Book
     @read = false
   end
 end
+
+class Author 
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+  
+end
