bf2ab2d37379c146fb8c2a13dc262443e9afaab2
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-08 16:40:52 +0000

    Automatically backed up by Learn

--- book.rb
+++ book.rb
@@ -1,9 +1,9 @@
 class Book
-  attr_accessor :title, :author, :read?
+  attr_accessor :title, :author, :read
 
   def initialize(title, author)
     @title = title
     @author = author
-    @read? = false
+    @read = false
   end
 end
